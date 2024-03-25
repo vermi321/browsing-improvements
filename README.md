fbd1a290b591a4e7c39ff11988b6016f18b4bc8f
vermi321 <143324589+vermi321@users.noreply.github.com>
2024-03-25 18:51:58 +0100

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # safer-twitter-extension
+
+#### DefiLlama extension (twitter part)
+https://github.com/DefiLlama/defillama-extension/blob/prod/src/pages/content/components/twitter.tsx
--- content-script.js
+++ content-script.js
@@ -1,9 +1,16 @@
 // Run the script in the same sandbox as a host site
-(() => {
-    const s = document.createElement('script');
-    s.src = chrome.runtime.getURL('twitter.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
+(async () => {
+    const load = async (scriptPath) => {
+        return new Promise(res => {
+            const s = document.createElement('script');
+            s.src = chrome.runtime.getURL(scriptPath);
+            s.onload = function () {
+                this.remove();
+                res();
+            };
+            (document.head || document.documentElement).appendChild(s);
+        })
+    }
+    await load('levenshtein.js');
+    await load('twitter.js');
 })();
